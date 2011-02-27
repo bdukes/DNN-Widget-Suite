6b9e6aa79ef5b0a51ce82acdf75bd976796d5bb6
SND\hismightiness_cp <SND\hismightiness_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2011-02-27 03:50:23 +0000

    Complete working version of the SyntaxHighlighter
    git-svn-id: https://dnnwidgets.svn.codeplex.com/svn@62919 81fbe566-5dc4-48c1-bdea-7421811ca204

--- trunk/WillStrohl.Widgets.SyntaxHighlighter.js
+++ trunk/WillStrohl.Widgets.SyntaxHighlighter.js
@@ -116,43 +116,39 @@ WillStrohl.Widgets.SyntaxHighlighter.prototype =
                 if (runDebug) $DEBUGLINE('Loading: ' + $dnn.baseResourcesUrl + 'Widgets/User/WillStrohl/js/SyntaxHighlighter/scripts/shAutoloader.js');
                 jQuery.getScript($dnn.baseResourcesUrl + 'Widgets/User/WillStrohl/js/SyntaxHighlighter/scripts/shAutoloader.js', function () {
 
-                    setTimeout(function () {
+                    /* add the supported brushes */
+                    if (runDebug) $DEBUGLINE('Load Brushes');
+                    SyntaxHighlighter.autoloader.apply(null, path(
+                        'applescript            @shBrushAppleScript.js',
+                        'actionscript3 as3      @shBrushAS3.js',
+                        'bash shell             @shBrushBash.js',
+                        'coldfusion cf          @shBrushColdFusion.js',
+                        'cpp c                  @shBrushCpp.js',
+                        'c# c-sharp csharp      @shBrushCSharp.js',
+                        'css                    @shBrushCss.js',
+                        'delphi pascal          @shBrushDelphi.js',
+                        'diff patch pas         @shBrushDiff.js',
+                        'erl erlang             @shBrushErlang.js',
+                        'groovy                 @shBrushGroovy.js',
+                        'java                   @shBrushJava.js',
+                        'jfx javafx             @shBrushJavaFX.js',
+                        'js jscript javascript  @shBrushJScript.js',
+                        'perl pl                @shBrushPerl.js',
+                        'php                    @shBrushPhp.js',
+                        'text plain             @shBrushPlain.js',
+                        'py python              @shBrushPython.js',
+                        'ruby rails ror rb      @shBrushRuby.js',
+                        'sass scss              @shBrushSass.js',
+                        'scala                  @shBrushScala.js',
+                        'sql                    @shBrushSql.js',
+                        'vb vbnet               @shBrushVb.js',
+                        'xml xhtml xslt html    @shBrushXml.js'
+                    ));
 
-                        /* add the supported brushes */
-                        if (runDebug) $DEBUGLINE('Load Brushes');
-                        SyntaxHighlighter.autoloader.apply(null, path(
-                            'applescript            @shBrushAppleScript.js',
-                            'actionscript3 as3      @shBrushAS3.js',
-                            'bash shell             @shBrushBash.js',
-                            'coldfusion cf          @shBrushColdFusion.js',
-                            'cpp c                  @shBrushCpp.js',
-                            'c# c-sharp csharp      @shBrushCSharp.js',
-                            'css                    @shBrushCss.js',
-                            'delphi pascal          @shBrushDelphi.js',
-                            'diff patch pas         @shBrushDiff.js',
-                            'erl erlang             @shBrushErlang.js',
-                            'groovy                 @shBrushGroovy.js',
-                            'java                   @shBrushJava.js',
-                            'jfx javafx             @shBrushJavaFX.js',
-                            'js jscript javascript  @shBrushJScript.js',
-                            'perl pl                @shBrushPerl.js',
-                            'php                    @shBrushPhp.js',
-                            'text plain             @shBrushPlain.js',
-                            'py python              @shBrushPython.js',
-                            'ruby rails ror rb      @shBrushRuby.js',
-                            'sass scss              @shBrushSass.js',
-                            'scala                  @shBrushScala.js',
-                            'sql                    @shBrushSql.js',
-                            'vb vbnet               @shBrushVb.js',
-                            'xml xhtml xslt html    @shBrushXml.js'
-                        ));
-
-                        /* highlight the code */
-                        if (runDebug) $DEBUGLINE('Calling SyntaxHighlighter.all();');
-                        SyntaxHighlighter.all();
-                        if (runDebug) $DEBUGLINE('Called SyntaxHighlighter.all();');
-
-                    }, 250);
+                    /* highlight the code */
+                    if (runDebug) $DEBUGLINE('Calling SyntaxHighlighter.all();');
+                    SyntaxHighlighter.all();
+                    if (runDebug) $DEBUGLINE('Called SyntaxHighlighter.all();');
 
                 });
 
@@ -170,7 +166,7 @@ WillStrohl.Widgets.SyntaxHighlighter.prototype =
 
 function path() {
     var args = arguments, result = [];
-    for(var i = 0; i < args.length; i++) result.push(args[i].replace('@', $dnn.baseResourceUrl + 'Widgets/User/WillStrohl/js/SyntaxHighlighter/scripts/'));
+    for(var i = 0; i < args.length; i++) result.push(args[i].replace('@', '/Resources/Widgets/User/WillStrohl/js/SyntaxHighlighter/scripts/'));
     return result;
 };
 
